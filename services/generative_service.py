308cc0c8ac43f2263d74b9d66207a3c6e4469b5d
Matthew Grady <matthewbgrady90@gmail.com>
2025-03-24 19:48:42 -0400

    chore/markov-configurable-stopwords

--- services/generative_service.py
+++ services/generative_service.py
@@ -1,7 +1,10 @@
 
 import random
+import re
 import requests
 import os
+
+MAX_MARKOV_CHAIN_LENGTH = 10
 
 # LLM Response Generator
 def generate_llm_response(message):
@@ -17,8 +20,28 @@ def generate_llm_response(message):
         }
         response = requests.post(url, headers=headers, json=data)
         json = response.json()
-        print(json['output'][0])
         if json and json['output'] and json['output'][0] and json['output'][0]['content'] and json['output'][0]['content'][0] and json['output'][0]['content'][0]['text']:
-            return json['output'][0]['content'][0]['text']
+            return re.sub(r'\n', '', json['output'][0]['content'][0]['text'])
         else:
             return None
+        
+
+def generate_markov_chain_response(deque):
+    if deque:
+        random_word = random.choice(deque.get_popular_keywords())[0]
+        response = random_word + " "
+        print(random_word, response)
+        for i in range(MAX_MARKOV_CHAIN_LENGTH):
+            try:
+                next_word = random.choice(deque.markov_chains[random_word])
+                if next_word:
+                    print(next_word)
+                    response += next_word + " "
+                else:
+                    next_word = random.choice(deque.markov_chains.keys())
+                    response += next_word + " "
+                random_word = next_word
+            except KeyError:
+                random_word = random.choice(deque.get_popular_keywords())[0]
+        return response
+
